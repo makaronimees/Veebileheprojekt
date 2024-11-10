8b555ab32f67c019c5f196554e8b51414cedfc5f
OpcodeMatrix <112854111+OpcodeMatrix@users.noreply.github.com>
2024-11-10 14:12:19 +0200

    Lisasin javascripti galerii jaoks

--- javascript.js
+++ javascript.js
@@ -1,1 +1,35 @@
+const pildid = [
+"https://varjupaik.ee/photos/suviste_large.jpg",
+"https://varjupaik.ee/photos/Rey_large.jpg",
+"https://varjupaik.ee/photos/Tsopp_large.jpg",
+"https://varjupaik.ee/photos/Lemsalu-1_large.jpg",
+"https://varjupaik.ee/photos/Anett%20Kontaveit_large.jpg",
+"https://varjupaik.ee/photos/Kanepi-1_large.jpg",
+"https://varjupaik.ee/photos/Ivanov-1_large.jpg"
+];
 
+var aktiivnePilt = 0;
+muudaGaleriiPilti(1);
+// Iga 5s järel muuda galerii pilti automaatselt
+var galeriiInterval = setInterval(()=>muudaGaleriiPilti(1), 5000);
+
+function muudaGaleriiPilti(dir, keelaAutomaatneMuutmine = false)
+{
+    // Kui kasutaja vajutab galerii pildi muutmiseks nuppe, lõpeta galerii pildi automaatne muutmine
+    if (keelaAutomaatneMuutmine)
+        clearInterval(galeriiInterval);
+    
+    // Muuda praeguse galerii pildi juurde käiv tekst nähtamatuks
+    document.getElementById(aktiivnePilt).style.display = "none";
+    
+    aktiivnePilt += dir;
+    // Muutuja aktiivnePilt peab jääma vahemikku [0, 6]
+    if (aktiivnePilt > 6)
+        aktiivnePilt = 0;
+    else if (aktiivnePilt < 0)
+        aktiivnePilt = 6;
+    
+    // Vaheta galerii pilt ja muuda selle juurde käiv tekst nähtavaks
+    document.getElementById("galeriiPilt").style.backgroundImage = "url(" + pildid[aktiivnePilt] + ")";
+    document.getElementById(aktiivnePilt).style.display = "block";
+}
