b347d7edc828e248fb5db9832c9e373be3adb86d
OpcodeMatrix <112854111+OpcodeMatrix@users.noreply.github.com>
2024-11-29 22:53:44 +0200

    Uuendasin javascripti

--- javascript.js
+++ javascript.js
@@ -1,35 +1,82 @@
-const pildid = [
-"https://varjupaik.ee/photos/suviste_large.jpg",
-"https://varjupaik.ee/photos/Rey_large.jpg",
-"https://varjupaik.ee/photos/Tsopp_large.jpg",
-"https://varjupaik.ee/photos/Lemsalu-1_large.jpg",
-"https://varjupaik.ee/photos/Anett%20Kontaveit_large.jpg",
-"https://varjupaik.ee/photos/Kanepi-1_large.jpg",
-"https://varjupaik.ee/photos/Ivanov-1_large.jpg"
-];
+var nupud = document.getElementsByClassName("nupp");
+var sisemineGalerii = document.getElementsByClassName("sisemineGalerii")[0];
 
-var aktiivnePilt = 0;
-muudaGaleriiPilti(1);
+var aktiivsePildiIndeks = 0;
 // Iga 5s järel muuda galerii pilti automaatselt
-var galeriiInterval = setInterval(()=>muudaGaleriiPilti(1), 5000);
+var galeriiInterval = setInterval(()=>valiJargminePilt(1, false), 5000);
 
-function muudaGaleriiPilti(dir, keelaAutomaatneMuutmine = false)
+function lahtesta(jargmisePildiIndeks)
 {
+    var aktiivnePilt = document.getElementById("pilt" + aktiivsePildiIndeks);
+    var jargminePilt = document.getElementById("pilt" + jargmisePildiIndeks);
+    // Muuda eelmine pilt nähtamatuks ja lähtesta praegu nähtava pildi järjekorranumber
+    aktiivnePilt.style.display = "none";
+    jargminePilt.style.order = "1";
+    // Lähtesta galerii positsioon, eemaldades sellelt klassid "parem" või "vasak"
+    sisemineGalerii.classList.remove("parem");
+    sisemineGalerii.classList.remove("vasak");
+    aktiivsePildiIndeks = jargmisePildiIndeks;
+
+    for (let nupp = 0; nupp < nupud.length; nupp++) {
+        nupud[nupp].disabled = false;
+    }
+}
+
+function muudaGaleriiPilti(jargmisePildiIndeks, keelaAutomaatneMuutmine = true)
+{
+    // Kui järgmine pilt on praegusest vasakul, on muutuja "suund" negatiivne, ja vastupidi
+    var suund = jargmisePildiIndeks - aktiivsePildiIndeks;
+    
+    if (suund == 0)
+        // Soovitakse laadida juba aktiivset pilti, seega pole vaja funktsiooniga jätkata
+        return;
+
+    // Kui järgmise ja praeguse pildi indeksite vahe on suurem galerii poolest pikkusest,
+    // muuda muutuja "suund" märk vastupidiseks
+    // Niiviisi liigutatakse galeriid alati sinna suunas, kus tal oleks pildini lühem tee
+    if (Math.abs(suund) >= 4)
+        suund *= -1;
+
+    for (let nupp = 0; nupp < nupud.length; nupp++) {
+        nupud[nupp].disabled = true;
+    }
+
     // Kui kasutaja vajutab galerii pildi muutmiseks nuppe, lõpeta galerii pildi automaatne muutmine
     if (keelaAutomaatneMuutmine)
         clearInterval(galeriiInterval);
+
+    var jargminePilt = document.getElementById("pilt" + jargmisePildiIndeks);
+    jargminePilt.style.display = "flex";
+
+    if (suund > 0)
+    {
+        // Liiguta järgmisena nähtavale ilmuv pilt praegu nähtavast pildist paremale ja lisa sisemisele galeriile
+        // klass "vasak", mis liigutab galeriid 1s jooksul ühe pildi võrra vasakule
+        jargminePilt.style.order = "2";
+        sisemineGalerii.classList.add("vasak");
+    }
+    else
+    {
+        // Liiguta järgmisena nähtavale ilmuv pilt praegu nähtavast pildist vasakule ja lisa sisemisele galeriile
+        // klass "parem", mis liigutab galeriid 1s jooksul ühe pildi võrra paremale
+        jargminePilt.style.order = "0";
+        sisemineGalerii.classList.add("parem");
+    }
+
+    document.getElementById("nupp" + aktiivsePildiIndeks).classList.remove("valikuNuppAktiivne");
+    document.getElementById("nupp" + jargmisePildiIndeks).classList.add("valikuNuppAktiivne");
     
-    // Muuda praeguse galerii pildi juurde käiv tekst nähtamatuks
-    document.getElementById(aktiivnePilt).style.display = "none";
-    
-    aktiivnePilt += dir;
-    // Muutuja aktiivnePilt peab jääma vahemikku [0, 6]
-    if (aktiivnePilt > 6)
-        aktiivnePilt = 0;
-    else if (aktiivnePilt < 0)
-        aktiivnePilt = 6;
-    
-    // Vaheta galerii pilt ja muuda selle juurde käiv tekst nähtavaks
-    document.getElementById("galeriiPilt").style.backgroundImage = "url(" + pildid[aktiivnePilt] + ")";
-    document.getElementById(aktiivnePilt).style.display = "block";
+    setTimeout(()=>lahtesta(jargmisePildiIndeks), 1000);
+}
+
+function valiJargminePilt(dir, keelaAutomaatneMuutmine = true)
+{
+    var jargmisePildiIndeks = aktiivsePildiIndeks + dir;
+    // Muutuja aktiivsePildiIndeks peab jääma vahemikku [0, 6]
+    if (jargmisePildiIndeks > 6)
+        jargmisePildiIndeks = 0;
+    else if (jargmisePildiIndeks < 0)
+        jargmisePildiIndeks = 6;
+
+    muudaGaleriiPilti(jargmisePildiIndeks, keelaAutomaatneMuutmine);
 }
